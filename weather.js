4c2a656897c7337128c6f83e461f75fa094126aa
Pavel <magma4810@gmail.com>
2023-10-21 17:11:48 +0300

    create functions

--- weather.js
+++ weather.js
@@ -1,49 +1,57 @@
-const APP_ID = "ab4639f5754271e773ed6d3ffd73f327";
-const storageKey = 'items';
-const listEl = document.querySelector('#list');
-const form = document.querySelector('form');
-const items = readList();
-//+
-function getText(){
-    const input = document.querySelector('#city');
-    return input.value; 
-}
-async function getWeather(){
-    let cityName = getText();
-    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${APP_ID}`);
-    const date = result.json();
-    console.log(date);
-    return date
-}
+(async function () {
+    const APP_ID = "ab4639f5754271e773ed6d3ffd73f327";
+    const storageKey = 'items';
+    const listEl = document.querySelector('#list');
+    const form = document.querySelector('form');
+    const items = await readList();
+    async function getWeather(items){
+        // console.log(items)
+        const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${items}&appid=${APP_ID}`);
+        // console.log(result)
+        const date = result.json();
+        // console.log(date);
+        return date;
+    }
 
-function addText(listEl){
-    const weather = getWeather();
-    listEl.innerHTML = `<ol>${`<li>${weather}</li>`}</ol>`;
-}
+    async function addText(listEl,items){
+        const weather = await getWeather(items);
+        const getWeatherTemp = JSON.stringify(weather, null,2)
+        console.log(getWeatherTemp.main.temp)//-----не выводится температура, пишет undefined
+        listEl.innerHTML = `<ol>${`<li>${getWeatherTemp}</li>`}</ol>`;
+    }
 
-function saveList(items){
-    localStorage.setItem(items,JSON.stringify(storageKey));
-}
+    function saveList(items){
+        localStorage.setItem(items,JSON.stringify(storageKey));
+    }
 
-function readList(){
-    const data = localStorage.getItem(storageKey);
-    return data === null ? [] : JSON.parse(data);
-}
+    async function readList(){
+        try{
+            const data = localStorage.getItem(storageKey);
+            return data === null ? [] : JSON.parse(data);
+        }catch(e){
+            console.error(e);
+            return [];
+        }
+        
+        
+    }
 
-form.addEventListener('submit',(ev)=>{
-//+
-    ev.preventDefault();
+    form.addEventListener('submit',(ev)=>{
+    //+
+        ev.preventDefault();
 
-    const formElement = ev.target;
-    console.log(formElement)
-    const input = formElement.querySelector('input');
-    const value = input.value;
-    input.value = '';
-    console.log(value)
-    items.push(value);
+        const formElement = ev.target;
+        // console.log(formElement)
+        const input = formElement.querySelector('input');
+        const value = input.value;
+        input.value = '';
+        // console.log(value)
+        items.push(value);
 
-    addText(listEl,items);
 
-    saveList(items);
-}
-)
+        addText(listEl,items);
+
+        saveList(items);
+    }
+    )
+})();
